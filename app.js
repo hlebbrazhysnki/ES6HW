d9a341dba13f5b76c0c56f04921de555594418f6
Hleb Brazhynski <hleb200217@gmail.com>
2021-10-18 12:11:01 -0400

    ES6HW

---
+++ app.js
@@ -0,0 +1,37 @@
+function getSum (a,b){
+    console.log( a + b );
+}
+getSum (10,5);
+
+let sum = (a,b) => a + b;
+(sum(10,5));
+
+
+
+
+function getSub (a,b){
+    console.log( a - b );
+}
+getSub (10,5);
+
+let sub = (a,b) => a - b;
+(sub(10,5));
+
+
+function getMul (a,b){
+    console.log( a * b );
+}
+getMul(10,5);
+
+let mul = (a,b) => a * b;
+(mul(10,5));
+
+
+function getDiv (a,b){
+    console.log( a / b );
+}
+getDiv(10,5);
+
+let div = (a,b) => a / b;
+(div(10,5));
+
